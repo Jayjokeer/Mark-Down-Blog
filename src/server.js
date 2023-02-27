7707a407b38e9fa4fa6c5d4a72ab142e6c992f55
john tom <johntommokaa@gmail.com>
2023-02-27 23:03:49 +0100

    new

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(methodOverride('_method'))
 
 //databse connection
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb://127.0.0.1:27017/blog',{
+mongoose.connect('mongodb+srv://johntom:<Machinegun>@markdn.ixw0snt.mongodb.net/test/blog',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 },
