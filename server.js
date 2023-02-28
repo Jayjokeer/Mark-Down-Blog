a9e581a3e7f13516754c3b599fd51bcd4217d57c
john tom <johntommokaa@gmail.com>
2023-02-28 18:57:58 +0100

    morre commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-const express = require('express');
-const PORT =process.env.PORT || 4000;
+
+const express = require('express')
+const PORT = 4000;
 const mongoose=require('mongoose')
 const ejs = require('ejs')
 const path = require ('path')
@@ -17,7 +17,7 @@ app.use(methodOverride('_method'))
 
 //databse connection
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb+srv://johntom:<Machinegun>@markdn.ixw0snt.mongodb.net/test/blog',{
+mongoose.connect('mongodb+srv://johntom:machinegun@cluster0.0up53ms.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 },
@@ -29,7 +29,21 @@ mongoose.connect('mongodb+srv://johntom:<Machinegun>@markdn.ixw0snt.mongodb.net/
         }
     }
     )
-    
+
+//database
+/*const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://johntom:machinegun@cluster0.0up53ms.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("Blog").collection("devices");
+  // perform actions on the collection object
+  client.close();
+  if(!err){
+    console.log('connected')
+  }
+});*/
+
 //routes
 app.use('/',require('./routes/router'))
 
